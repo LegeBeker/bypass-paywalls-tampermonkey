5a4855227e5edaa69e9f407f37b8570b52dbec2b
amraks85 <43678213+amraks85@users.noreply.github.com>
2018-09-30 15:55:46 +1000

    background.js
    added the advocate and examiner

--- background.js
+++ background.js
@@ -43,7 +43,9 @@ var defaultSites = {
   'The Sydney Morning Herald': 'smh.com.au',
   'The Washington Post': 'washingtonpost.com',
   'The Wall Street Journal': 'wsj.com',
-  'Wired': 'wired.com'
+  'Wired': 'wired.com',
+  'The Advocate': 'theadvocate.com.au',
+  'Examiner': 'examiner.com.au'
 };
 
 const restrictions = {
@@ -78,7 +80,9 @@ const remove_cookies = [
 'bostonglobe.com',
 'wired.com',
 'denverpost.com',
-'chicagobusiness.com'
+'chicagobusiness.com',
+'The Advocate': 'theadvocate.com.au',
+'Examiner': 'examiner.com.au'
 ]
 
 function setDefaultOptions() {
