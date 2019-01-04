39cf6c4eb262e1ef8bf6e172aa64f4ddda0765f7
iscreamcoke <adrien.colliard@gmail.com>
2019-01-04 19:32:34 +0100

    Updated to add Harper's Magazine

--- options.js
+++ options.js
@@ -17,6 +17,7 @@ var defaultSites = {
   'Glassdoor': 'glassdoor.com',
   'Haaretz': 'haaretz.co.il',
   'Haaretz English': 'haaretz.com',
+  'Harper\'s Mag': 'harpers.org',
   'Hartford Courant': 'courant.com',
   'Harvard Business Review': 'hbr.org',
   'Inc.com': 'inc.com',
