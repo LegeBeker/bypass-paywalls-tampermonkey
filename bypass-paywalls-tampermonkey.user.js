687f592ce41823c248f1094c4344438b46f3efc0
LegeBeker <volkanwelp@gmail.com>
2022-05-28 09:53:42 +0200

    🚚 Rename for install and added individual matches

--- bypass-paywalls-tampermonkey.user.js
+++ bypass-paywalls-tampermonkey.user.js
@@ -4,10 +4,180 @@
 // @author       LegeBeker (https://github.com/LegeBeker), (Fork from https://github.com/iamadamdev/bypass-paywalls-chrome)
 // @namespace    https://github.com/LegeBeker/bypass-paywalls-tampermonkey/
 // @supportURL   https://github.com/LegeBeker/bypass-paywalls-tampermonkey/issues
+// @updateURL    https://github.com/LegeBeker/bypass-paywalls-tampermonkey/raw/master/bypass-paywalls-tampermonkey.js
+// @downloadURL  https://github.com/LegeBeker/bypass-paywalls-tampermonkey/raw/master/bypass-paywalls-tampermonkey.js
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAIAAAACACAQAAABpN6lAAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAJcEhZcwAACxMAAAsTAQCanBgAAAAHdElNRQfkBhAPLAM1PCwiAAABZ0lEQVR42u3aPUoDURiG0bsBU6W2UKLrMQoWrkIY3Yu9INhaBldidBNGrJLXQhS9MYU4guM9TzkzCdyT+flIUookSZIkSVJV/nkAAAAAAABAywC9iw1srgEAAAAAAAAAbF7gd/f3PZn99vsBAAAAAAAAAPoD+PFgMvRBCAAAAAAAAADQG0DzgxAAAAAAAADg+wCTIAAAAAAAAOD/AX4ZAgAAAAAAAP4QwNACAAAAAAAAABiEAAAAAAAAAAAGIQAAmgdYVFu2Ph0+qvYuBr78ej2PJffVpoNPLzis9j4MHKBez7zktt6U8fvh48yrvTeDXv76emYl52uPznmmGWWUw7WzIzkd8Mn/1Xq6kv2s0mrLTEopuWoW4PL15NjNU5PLX2Tn7fqYNngZrHL88RZx1hjBKl19lzxq6EJ4zslXD4rtXGTZwGd/nd3NT8u9dJnlbm1A/g+3vLvM0mVSJEmSJEmSJElSo70AqgAJADOYlfQAAAAldEVYdGRhdGU6Y3JlYXRlADIwMjAtMDYtMTZUMTU6NDM6NTcrMDA6MDAT/mVIAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDIwLTA2LTE2VDE1OjQzOjU3KzAwOjAwYqPd9AAAAABJRU5ErkJggg==
 // @run-at       document-end
-// @version      0.1.0
-// @match        *://*/*
+// @version      0.1.1
+// @match        *://*.adweek.com/*
+// @match        *://*.ad.nl/*
+// @match        *://*.americanbanker.com/*
+// @match        *://*.ambito.com/*
+// @match        *://*.baltimoresun.com/*
+// @match        *://*.barrons.com/*
+// @match        *://*.bloombergquint.com/*
+// @match        *://*.bloomberg.com/*
+// @match        *://*.bndestem.nl/*
+// @match        *://*.bostonglobe.com/*
+// @match        *://*.bd.nl/*
+// @match        *://*.brisbanetimes.com.au/*
+// @match        *://*.businessinsider.com/*
+// @match        *://*.caixinglobal.com/*
+// @match        *://*.centralwesterndaily.com.au/*
+// @match        *://*.acs.org/*
+// @match        *://*.chicagotribune.com/*
+// @match        *://*.corriere.it/*
+// @match        *://*.chicagobusiness.com/*
+// @match        *://*.dailypress.com/*
+// @match        *://*.gelderlander.nl/*
+// @match        *://*.groene.nl/*
+// @match        *://*.destentor.nl/*
+// @match        *://*.speld.nl/*
+// @match        *://*.tijd.be/*
+// @match        *://*.volkskrant.nl/*
+// @match        *://*.demorgen.be/*
+// @match        *://*.denverpost.com/*
+// @match        *://*.df.cl/*
+// @match        *://*.editorialedomani.it/*
+// @match        *://*.dynamed.com/*
+// @match        *://*.ed.nl/*
+// @match        *://*.elmercurio.com/*
+// @match        *://*.elpais.com/*
+// @match        *://*.elperiodico.com/*
+// @match        *://*.elu24.ee/*
+// @match        *://*.britannica.com/*
+// @match        *://*.estadao.com.br/*
+// @match        *://*.examiner.com.au/*
+// @match        *://*.expansion.com/*
+// @match        *://*.fnlondon.com/*
+// @match        *://*.financialpost.com/*
+// @match        *://*.ft.com/*
+// @match        *://*.firstthings.com/*
+// @match        *://*.foreignpolicy.com/*
+// @match        *://*.fortune.com/*
+// @match        *://*.genomeweb.com/*
+// @match        *://*.glassdoor.com/*
+// @match        *://*.globes.co.il/*
+// @match        *://*.grubstreet.com/*
+// @match        *://*.haaretz.co.il/*
+// @match        *://*.haaretz.com/*
+// @match        *://*.handelsblatt.com/*
+// @match        *://*.harpers.org/*
+// @match        *://*.courant.com/*
+// @match        *://*.hbr.org/*
+// @match        *://*.heraldsun.com.au/*
+// @match        *://*.fd.nl/*
+// @match        *://*.historyextra.com/*
+// @match        *://*.humo.be/*
+// @match        *://*.ilmanifesto.it/*
+// @match        *://*.inc.com/*
+// @match        *://*.interest.co.nz/*
+// @match        *://*.investorschronicle.co.uk)
+// @match        *://*.lecho.be/*
+// @match        *://*.labusinessjournal.com/*
+// @match        *://*.lanacion.com.ar/*
+// @match        *://*.repubblica.it/*
+// @match        *://*.lastampa.it/*
+// @match        *://*.latercera.com/*
+// @match        *://*.lavoixdunord.fr/*
+// @match        *://*.ledevoir.com/*
+// @match        *://*.leparisien.fr/*
+// @match        *://*.lesechos.fr/*
+// @match        *://*.loebclassics.com/*
+// @match        *://*.lrb.co.uk/*
+// @match        *://*.latimes.com/*
+// @match        *://*.mit.edu/*
+// @match        *://*.technologyreview.com/*
+// @match        *://*.medium.com/*
+// @match        *://*.medscape.com/*
+// @match        *://*.mexiconewsdaily.com/*
+// @match        *://*.mv-voice.com/*
+// @match        *://*.nationalgeographic.com/*
+// @match        *://*.nydailynews.com/*
+// @match        *://*.nrc.nl/*
+// @match        *://*.ntnews.com.au/*
+// @match        *://*.nationalpost.com/*
+// @match        *://*.nzz.ch/*
+// @match        *://*.nymag.com/*
+// @match        *://*.nzherald.co.nz/*
+// @match        *://*.ocregister.com/*
+// @match        *://*.orlandosentinel.com/*
+// @match        *://*.pzc.nl/*
+// @match        *://*.paloaltoonline.com/*
+// @match        *://*.parool.nl/*
+// @match        *://*.postimees.ee/*
+// @match        *://*.qz.com/*
+// @match        *://*.quora.com/*
+// @match        *://*.gelocal.it/*
+// @match        *://*.republic.ru/*
+// @match        *://*.reuters.com/*
+// @match        *://*.sandiegouniontribune.com/*
+// @match        *://*.sfchronicle.com/*
+// @match        *://*.scientificamerican.com/*
+// @match        *://*.seekingalpha.com/*
+// @match        *://*.slate.com/*
+// @match        *://*.sofrep.com/*
+// @match        *://*.statista.com/*
+// @match        *://*.startribune.com/*
+// @match        *://*.stuff.co.nz/*
+// @match        *://*.sun-sentinel.com/*
+// @match        *://*.techinasia.com/*
+// @match        *://*.telegraaf.nl/*
+// @match        *://*.adelaidenow.com.au/*
+// @match        *://*.theadvocate.com.au/*
+// @match        *://*.theage.com.au/*
+// @match        *://*.the-american-interest.com/*
+// @match        *://*.theathletic.com/*
+// @match        *://*.theathletic.co.uk/*
+// @match        *://*.theatlantic.com/*
+// @match        *://*.afr.com/*
+// @match        *://*.theaustralian.com.au/*
+// @match        *://*.bizjournals.com/*
+// @match        *://*.canberratimes.com.au/*
+// @match        *://*.thecourier.com.au/*
+// @match        *://*.couriermail.com.au/*
+// @match        *://*.thecut.com/*
+// @match        *://*.dailytelegraph.com.au/*
+// @match        *://*.thediplomat.com/*
+// @match        *://*.economist.com/*
+// @match        *://*.theglobeandmail.com/*
+// @match        *://*.theherald.com.au/*
+// @match        *://*.thehindu.com/*
+// @match        *://*.irishtimes.com/*
+// @match        *://*.kansascity.com/*
+// @match        *://*.mercurynews.com/*
+// @match        *://*.themercury.com.au/*
+// @match        *://*.mcall.com/*
+// @match        *://*.thenation.com/*
+// @match        *://*.thenational.scot/*
+// @match        *://*.newstatesman.com/*
+// @match        *://*.nytimes.com/*
+// @match        *://*.newyorker.com/*
+// @match        *://*.news-gazette.com/*
+// @match        *://*.theolivepress.es/*
+// @match        *://*.inquirer.com/*
+// @match        *://*.thesaturdaypaper.com.au/*
+// @match        *://*.seattletimes.com/*
+// @match        *://*.spectator.com.au/*
+// @match        *://*.spectator.co.uk/*
+// @match        *://*.smh.com.au/*
+// @match        *://*.telegraph.co.uk/*
+// @match        *://*.thestar.com/*
+// @match        *://*.wsj.com/*
+// @match        *://*.washingtonpost.com/*
+// @match        *://*.thewrap.com/*
+// @match        *://*.themarker.com/*
+// @match        *://*.the-tls.co.uk/*
+// @match        *://*.towardsdatascience.com/*
+// @match        *://*.trouw.nl/*
+// @match        *://*.tubantia.nl/*
+// @match        *://*.vanityfair.com/*
+// @match        *://*.vn.nl/*
+// @match        *://*.vulture.com/*
+// @match        *://*.journalnow.com/*
+// @match        *://*.wired.com/*
+// @match        *://*.worldpoliticsreview.com/*
+// @match        *://*.zeit.de/*
 // ==/UserScript==
 
 (function () {
@@ -1328,14 +1498,11 @@
     }
 
     function matchUrlDomain(domains, url) {
-        return matchDomain(domains, urlHost(url));
-    }
-
-    function matchDomain(domains, hostname) {
         let matchedDomain = false;
-        if (!hostname) { hostname = window.location.hostname; }
+        let url_host = urlHost(url);
+        if (!url_host) { url_host = window.location.hostname; }
         if (typeof domains === 'string') { domains = [domains]; }
-        domains.some(domain => (hostname === domain || hostname.endsWith('.' + domain)) && (matchedDomain = domain));
+        domains.some(domain => (url_host === domain || url_host.endsWith('.' + domain)) && (matchedDomain = domain));
         return matchedDomain;
     }
 
