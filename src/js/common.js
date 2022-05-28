1c22bf4c2dfb23944fcb30b034c5d6578e6252d7
LegeBeker <volkanwelp@gmail.com>
2022-05-28 09:11:21 +0200

    🔥 Removed unnecessary files

--- src/js/common.js
+++
@@ -1,8 +0,0 @@
-const extensionApi =
-    (typeof browser === 'object' &&
-     typeof browser.runtime === 'object' &&
-     typeof browser.runtime.getManifest === 'function') ? browser
-      : (typeof chrome === 'object' &&
-     typeof chrome.runtime === 'object' &&
-     typeof chrome.runtime.getManifest === 'function') ? chrome
-        : console.log('Cannot find extensionApi under namespace "browser" or "chrome"');
--- src/js/version.js
+++
@@ -1,3 +0,0 @@
-const manifestData = extensionApi.runtime.getManifest();
-const versionString = 'v' + manifestData.version;
-document.getElementById('version').innerText = versionString;
